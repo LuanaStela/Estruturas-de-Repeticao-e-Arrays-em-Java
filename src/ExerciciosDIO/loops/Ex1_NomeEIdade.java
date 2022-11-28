79c06a19bc09059dde96bcd8eff0fcebc4137df7
Luana Stela <luana.arantes.lsa@gmail.com>
2022-11-28 12:44:43 -0300

    Exercício resolvido.

--- src/ExerciciosDIO/loops/Ex1_NomeEIdade.java
+++ src/ExerciciosDIO/loops/Ex1_NomeEIdade.java
@@ -1,5 +1,28 @@
 package ExerciciosDIO.loops;
 
-public class Ex1_NomeEIdade {
+import java.util.Scanner;
 
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome de um aluno, e o segundo representando sua idade.
+(Pare o programa inserindo o valor 0 no campo nome.)
+ */
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui...");
+
+    }
 }
