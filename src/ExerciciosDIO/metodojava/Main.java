adcd5a2c91982184a7f54ef6589562afef0a42a7
Luana Stela <luana.arantes.lsa@gmail.com>
2022-11-30 15:57:09 -0300

    Exercícios práticos.

---
+++ src/ExerciciosDIO/metodojava/Main.java
@@ -0,0 +1,28 @@
+package ExerciciosDIO.metodojava;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercício calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        //Empréstimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, 2);
+        Emprestimo.calcular(1000, 3);
+        Emprestimo.calcular(1000, 5);
+
+    }
+}
