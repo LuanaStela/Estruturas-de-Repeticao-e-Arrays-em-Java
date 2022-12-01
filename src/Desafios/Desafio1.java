a3d1ec4c8699775c5d7ce0b5d93da5469f932aac
Luana Stela <luana.arantes.lsa@gmail.com>
2022-12-01 14:58:58 -0300

    Exercícios práticos.

---
+++ src/Desafios/Desafio1.java
@@ -0,0 +1,27 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class Desafio1 {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int idade1 = leitor.nextInt();
+        int idade2 = leitor.nextInt();
+        int idade3 = leitor.nextInt();
+
+        int soma = (idade1 + idade2 + idade3)/3;
+
+
+        if (soma < 25) {
+            System.out.println("Jovens!");
+        } else
+            if (soma > 26 && soma < 60) {
+                System.out.println("Adultos!");
+            
+        } else {
+                System.out.println("Idoso!");
+            }
+    }
+}
