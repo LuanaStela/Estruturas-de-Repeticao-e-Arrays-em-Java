2cde40f43a51e99ed66022ba26ca52e5d7116d5b
Luana Stela <luana.arantes.lsa@gmail.com>
2022-12-20 19:24:49 -0300

    Atualização.

--- src/DesafiosIntermediarios/DesafioNumeroFeliz.java
+++ src/DesafiosIntermediarios/DesafioNumeroFeliz.java
@@ -6,5 +6,32 @@ public class DesafioNumeroFeliz {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
+
+        scan.close();
+        System.out.println(numeroFeliz(n));
+    }
+    public static boolean numeroFeliz(int n) {
+
+        String texto = String.valueOf(n);
+
+        if (texto.length() == 1) {
+            return Integer.parseInt(texto) == 1;
+        }
+
+        do {
+            String[] vetor = texto.split("");
+            int soma = 0;
+            for (int x = 0; x < vetor.length; x++) {
+                int valor = Integer.parseInt(vetor[x]);
+                soma += (valor * valor);
+            }
+
+            String somaStr = String.valueOf(soma);
+            if (somaStr.length() == 1) {
+                return Integer.parseInt(somaStr) == 1;
+            } texto = somaStr;
+
+        } while (texto.length() != 1);
+        return false;
     }
 }
