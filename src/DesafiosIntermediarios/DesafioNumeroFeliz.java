3dcaf8801f926ad1220d4eb48849c33eaa185317
Luana Stela <luana.arantes.lsa@gmail.com>
2022-12-20 19:17:00 -0300

    Atualização

---
+++ src/DesafiosIntermediarios/DesafioNumeroFeliz.java
@@ -0,0 +1,10 @@
+package DesafiosIntermediarios;
+
+import java.util.Scanner;
+
+public class DesafioNumeroFeliz {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+    }
+}
