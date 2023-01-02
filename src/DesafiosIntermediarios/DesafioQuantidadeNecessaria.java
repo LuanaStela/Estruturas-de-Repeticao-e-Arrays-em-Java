da89e0b5bca0661acf45d7d86e0e9ca3e2472c08
Luana Stela <luana.arantes.lsa@gmail.com>
2023-01-02 09:50:04 -0300

    Atualização.

---
+++ src/DesafiosIntermediarios/DesafioQuantidadeNecessaria.java
@@ -0,0 +1,23 @@
+package DesafiosIntermediarios;
+
+import java.util.Scanner;
+
+public class DesafioQuantidadeNecessaria {
+    public static void main(String[] args) {
+        final int SLICE = 4;
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+        for(int i = 1; i <= T; i++) {
+            double amigos = scanner.nextInt() * scanner.nextInt();
+            double nPizzas = Math.ceil(amigos / SLICE);
+
+            System.out.println((int)nPizzas);
+        }
+        scanner.close();
+
+    }
+
+}
+
---
+++ src/DesafiosIntermediarios/DiferencaEntreDigitos.java
@@ -0,0 +1,28 @@
+package DesafiosIntermediarios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DiferencaEntreDigitos {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero = scan.nextInt();
+
+        String numeroEmString = String.valueOf(numero);
+        String[] split = numeroEmString.split("");
+
+        int soma = 0;
+        int multiplicacao = 1;
+        int diferenca = 0;
+
+        for (int i = 0; i < split.length; i ++){
+            int item = Integer.parseInt(split[i]);
+            soma = soma + item;
+            multiplicacao = multiplicacao * item;
+        }
+
+        diferenca = multiplicacao - soma;
+        System.out.println(diferenca);
+    }
+}
